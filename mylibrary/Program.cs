9456dcf2bf3f5e0b05c879a55ddb7766105eb933
Fitalew <ftalewabate145@gmail.com>
2025-06-01 11:45:51 -0900

    Add files via upload

---
+++ mylibrary/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace MyLibrary
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Database.InitializeDatabase();  
+
+            Application.Run(new LoginForm()); 
+        }
+    }
+}
