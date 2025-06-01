c0337f96df38bf839dc6a4b50f347c8925eb5d0e
Fitalew <ftalewabate145@gmail.com>
2025-06-01 11:49:24 -0900

    Add files via upload

---
+++ mylibrary/MainForm.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace MyLibrary
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent(); 
+            this.Load += MainForm_Load; 
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            
+            
+        }
+
+        private void btnTestConnection_Click(object sender, EventArgs e)
+        {
+            TestConnectionForm form = new TestConnectionForm();
+            form.ShowDialog();
+        }
+
+        private void btnBooksManagement_Click(object sender, EventArgs e)
+        {
+            BookForm bookForm = new BookForm();
+            bookForm.ShowDialog();
+        }
+
+        private void btnBorrowersManagement_Click(object sender, EventArgs e)
+        {
+            BorrowersForm borrowersForm = new BorrowersForm();
+            borrowersForm.ShowDialog();
+        }
+    }
+}
